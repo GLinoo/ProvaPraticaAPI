c83ea4efe51312fdc0de83f85603a0671edd2132
GABRIEL LINO VASCONCELLOS DOS SANTOS <gabriel_lv_santos@estudante.sesisenai.org.br>
2025-05-13 14:35:38 -0300

    Express instalado e servidor configurado

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid');
-
+const express = require('express')
+const app = express();
+const PORT = 3000
 
 function addLogs(nome){
     const data = new Date().toISOString()
     fs.appendFile('logs.txt', uuidv4() , data, nome)   
 }
+
+app.use(express())
+
+
+
+app.listen(PORT, () => {
+    console.log('Servidor rodando na porta: ', PORT)
+})
 
 
 
