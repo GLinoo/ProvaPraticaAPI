95233a29d7bdf4ef3898ceb6ef522a27ce7e4b53
GABRIEL LINO VASCONCELLOS DOS SANTOS <gabriel_lv_santos@estudante.sesisenai.org.br>
2025-05-13 16:57:41 -0300

    Rota get id criada

--- server.js
+++ server.js
@@ -1,37 +1,52 @@
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid');
-const express = require('express')
+const express = require('express');
+const { log } = require('console');
 const app = express();
 const PORT = 3000
 
 function addLogs(id , nome){
-    const data = new Date().toISOString()
-    fs.appendFile('logs.txt', id , data, nome)   
+    const date = new Date().toISOString()
+    const mensagem = `${id} ${date} ${JSON.stringify(nome)}\n`
+    fs.appendFile('logs.txt', mensagem, (err) => {
+        if(err) {
+            console.log('Erro ao salvar log', err)
+        }
+    })
 }
 
-app.use(express())
+app.use(express.json())
 
 app.post('/logs', (req,res) =>{
-    const {nome} = req.body
+    const nome = req.body
     if(!nome){
-      res.status(400).json({message: 'O campo "nome" deve ser preenchido!'})  
+      return res.status(400).json({message: 'O campo "nome" deve ser preenchido!'})  
     }
-    const id = uuidv4()
-    addLogs(id,nome)
-    return res.status(200).json({message: 'Log adicionada com sucesso', id})
+    const idCriado = uuidv4()
+    addLogs(idCriado,nome)
+    return res.status(200).json({message: 'Log adicionada com sucesso', idCriado})
 
+})
+
+app.get('/logs/:id', (req,res) => {
+    const id = req.params.id
+    fs.readFile('logs.txt', 'utf8', (err, data) =>{
+        if(err){
+            return res.status(500).json({message: "Erro ao ler arquivo de logs", err})
+        }
+         const linha = data
+         .split('\n')
+         .find(l => l.startsWith(id + ' '))
+
+         if(!linha){
+            return res.status(404).json({message: "Erro ao ler logs"})
+         }
+         const [logID, dataLog, ...nome] = linha.split(' ')
+         res.status(200).json({log: logID, data: dataLog, nome: nome})
+    } 
+    )
 })
 
 app.listen(PORT, () => {
     console.log('Servidor rodando na porta: ', PORT)
 })
-
-
-
-
-
-
-
-
-
-
