30474044d19367c045e90f26ee8462b06debdfb2
GABRIEL LINO VASCONCELLOS DOS SANTOS <gabriel_lv_santos@estudante.sesisenai.org.br>
2025-05-13 14:48:52 -0300

    Metodo post com rota /logs criado

--- server.js
+++ server.js
@@ -4,14 +4,23 @@ const express = require('express')
 const app = express();
 const PORT = 3000
 
-function addLogs(nome){
+function addLogs(id , nome){
     const data = new Date().toISOString()
-    fs.appendFile('logs.txt', uuidv4() , data, nome)   
+    fs.appendFile('logs.txt', id , data, nome)   
 }
 
 app.use(express())
 
+app.post('/logs', (req,res) =>{
+    const {nome} = req.body
+    if(!nome){
+      res.status(400).json({message: 'O campo "nome" deve ser preenchido!'})  
+    }
+    const id = uuidv4()
+    addLogs(id,nome)
+    return res.status(200).json({message: 'Log adicionada com sucesso', id})
 
+})
 
 app.listen(PORT, () => {
     console.log('Servidor rodando na porta: ', PORT)
